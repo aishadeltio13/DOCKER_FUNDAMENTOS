a66337b1cbb729d10f8a0c7139e75a1188ba50da
Aisha del Tio <adeltiodeprado@gmail.com>
2025-10-11 13:40:32 +0200

    Update
    Explicación de cómo trabajar con input cuando trabajamos con docker

--- docker2/main_docker2.py
+++ docker2/main_docker2.py
@@ -1,4 +1,6 @@
 print('Hello container, intento2')
 
+# Tenemos un input, entonces para que nos funcione la primera vez que hagamos run --> docker run -it --name contendor imagen
+# Para volver a ejecutarlo y que nos salga el input --> docker start -ai contenedor
 a= input("Nombre")
 print(a) 
