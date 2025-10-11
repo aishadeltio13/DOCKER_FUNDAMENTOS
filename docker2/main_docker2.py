0833329c08834a23658cd654232f7f44ef03f08d
Aisha del Tio <adeltiodeprado@gmail.com>
2025-10-11 13:47:22 +0200

    update
    espacio en nombre para que no salga pegado

--- docker2/main_docker2.py
+++ docker2/main_docker2.py
@@ -2,5 +2,5 @@ print('Hello container, intento2')
 
 # Tenemos un input, entonces para que nos funcione la primera vez que hagamos run --> docker run -it --name contendor imagen
 # Para volver a ejecutarlo y que nos salga el input --> docker start -ai contenedor
-a= input("Nombre")
-print(a) 
+a= input("Nombre ")
+print(a)
