0504f35dae546d0bdf37966b644c34aa68063925
Aisha del Tio <adeltiodeprado@gmail.com>
2025-10-18 12:00:15 +0200

    docker 8 y docker 9
    meter librerias con compose
    usar dos scripts en un compose

---
+++ docker7/main_docker7.py
@@ -0,0 +1,10 @@
+def primera_letra(nombre):
+    """
+    Devuelve un mensaje con la primera letra del nombre dado.
+    """
+    return f'La primera letra de {nombre} es {nombre[0]}'
+
+# Ejemplo de uso:
+name = "Laura"
+print(primera_letra(name))
+
---
+++ docker8/main_docker8.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+arr1 = np.array([1, 3, 5, 7, 9])
+arr2 = np.array([2, 4, 6, 8, 10])
+
+# Suma elemento por elemento
+suma = arr1 + arr2
+
+print("Array 1:", arr1)
+print("Array 2:", arr2)
+print("Suma de ambos arrays:", suma)
