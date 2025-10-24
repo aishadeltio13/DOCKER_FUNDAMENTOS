8a8f3b99040fd75d7f6079f9346167861444b367
Aisha del Tio <adeltiodeprado@gmail.com>
2025-10-24 16:11:14 +0200

    dewf

---
+++ docker10/main.py
@@ -0,0 +1,6 @@
+def resta(a,b):
+    c= a-b
+    return c
+
+print(resta(5,3))
+
---
+++ docker11/espar.py
@@ -0,0 +1,7 @@
+def espar(a):
+    if a % 2 == 0:
+        return f'Es par'
+    else:
+        return f'Es impar'
+    
+print(espar(5))
